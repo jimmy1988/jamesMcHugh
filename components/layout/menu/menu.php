3ad39e5b3030dc821aa80a8291a23c4d3590aa70
James McHugh <james.mchugh1988@gmail.com>
2017-02-26 00:24:58 +0000

    modified the menu bar and started to modify the index page to make the inital greeting more professional

--- components/layout/menu/menu.php
+++ components/layout/menu/menu.php
@@ -2,15 +2,15 @@
 <?php
   //hamburger button located on top.php
 ?>
-<link type="text/css" rel="stylesheet" href="components/layout/menu/css/menu.css">
+<link type="text/css" rel="stylesheet" href="components/layout/menu/css/menu.css?<?php echo date('d-m-Y_h:i:s');?>">
 <!--Import javascript-->
 <script type="text/javascript" src="js/hamburger.js"></script>
 <div id="mainMenu">
   <ul class="menu">
     <li><a href="index.php" title="Go to the Home Page"><i class="fa fa-home"></i></a></li>
-    <li><a href="newBookingLogin.php?cont=newBooking" title="Make a New Booking">New Booking</a></li>
+    <!-- <li><a href="newBookingLogin.php?cont=newBooking" title="Make a New Booking">New Booking</a></li> -->
     <li><a href="#" title="See All Services and Prices">Prices &amp; Services</a></li>
-    <li><a href="aboutMe.php" title="Look at my Biography">Bio</a></li>
+    <li><a href="aboutMe.php" title="Look at my Biography">About Me</a></li>
     <li><a href="#" title="See my Portfolio">Portfolio</a>
     <li><a href="contactMe.php" title="Contact Me">Contact Me</a></li>
   </ul>
--- components/layout/top/top.php
+++ components/layout/top/top.php
@@ -1,5 +1,5 @@
 <!--Start of the header-->
-<link type="text/css" rel="stylesheet" href="components/layout/top/css/top.css">
+<link type="text/css" rel="stylesheet" href="components/layout/top/css/top.css?<?php echo date('d-m-Y_h:i:s');?>">
 <div class="main" id="header">
   <div class="inner" id="innerHeader">
     <div id="hamburger">
@@ -8,8 +8,8 @@
       <div class="bar"></div>
       <div class="bar"></div>
     </div>
-    <div class="left">
-      <h1><a href="index.php" title="Go to the Home Page">James McHugh</a></h1>
+    <div id="logo">
+      <h1><a href="index.php" title="Go to the Home Page">JM</a></h1>
     </div>
   </div>
 </div>
