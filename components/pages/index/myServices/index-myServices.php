93d7052ad09104f6ec7730ce2b814fe615e6b076
James McHugh <james.mchugh1988@gmail.com>
2017-02-26 17:36:47 +0000

    added consulation information to index

--- components/pages/index/myServices/index-myServices.php
+++ components/pages/index/myServices/index-myServices.php
@@ -26,6 +26,34 @@
         <div>Website &amp; Development Advice</div>
       </a>
     </div>
+    <div id="consultationAndProjectInformation">
+      <h4 id="consultationInformationTitle">Consultations <a href="#" id="openCloseConsulations">+</a></h4>
+      <!--On load, close - use JS to open and close the div - change link from + to - and back again -->
+      <p id="consultationInformation">
+        Consultations are free.
+        <br/>
+        During the consultation process I will showcase some of the projects which I have completed so far including some of the clients I have designed sites and web applications for, after this we then move onto discuss all of the services offered and what is charged for each service including the hourly rate.
+        <br/>
+        If any discounts or promotions are running the information will be made available to you at the time of sale.
+        Once we have discussed what I can offer you, I will then ask for a summary of what your idea or business is about and how I can build solutions within technology to help you with your day to day tasks and promote your business idea.
+        <br/>
+        Once the meeting is completed you will have 14 working days to decide whether I am the perfect person for your project, if I am we will then start by to move into the specification and design phase. BUT FIRST! We have to sign contracts, disclosure forms and devise a cost invoice for the projects along with timescales and a way of tracking progress on the project.
+        <br/>
+        Don't worry if all of this is daunting at first, I will be here to guide you through every step of the project and advise your where neccessary.<br/>
+        <br/>
+        REMEMBER! I am here to advice you of what you need to turn your idea into a reality. All advice is free but should be taken with care as the information is subject to change with the grwoing technology market.
+        <br/>
+        HELPFUL HINT!: It is advisable to bring with you any materials that could help with the design and specification process and eventually the building of the project.
+        If you do not bring these with you it could cause delays to the project, which may incur additional fees.
+        Examples of these are sketches or drawings of a design, flowcharts of how processes need to be completed and any studies you may have completed prior to consultation.
+        <br/>
+      </p>
+      <h4 id="projectProcessInformationTitle">How does a project proceed? <a href="#" id="openCloseConsulations">+</a></h4>
+        <!--On load, close - use JS to open and close the div - change link from + to - and back again -->
+      <p id="projectInformation">
+        
+      </p>
+    </div>
   </div>
 </div>
 <!--End of Our Services-->
