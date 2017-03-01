525cb0ecb33d18a05200a8c4994408c4562abac3
James McHugh <james.mchugh1988@gmail.com>
2017-03-01 11:59:45 +0000

    added more links into index.php

--- components/pages/index/myServices/index-myServices.php
+++ components/pages/index/myServices/index-myServices.php
@@ -5,23 +5,23 @@
   <div class="inner" id="innerMyServices">
     <h3 id="title">MY SERVICES</h3>
     <div id="align">
-      <a href="construction.html" class="yes" id="planning" title="See Service Details and Prices">
+      <a href="pricesAndServices.php" class="yes" id="planning" title="See Service Details and Prices">
         <i class="fa fa-book"></i><i class="fa fa-code"></i>
         <div>Website Planning, Design, Coding &amp; Testing</div>
       </a>
-      <a href="construction.html" class="yes" id="userInterface" title="See Service Details and Prices">
+      <a href="pricesAndServices.php" class="yes" id="userInterface" title="See Service Details and Prices">
         <i class="fa fa-user"></i>
         <div>User Interface Design &amp; Testing</div>
       </a>
-      <a href="construction.html" class="yes" id="userExperience" title="See Service Details and Prices">
+      <a href="pricesAndServices.php" class="yes" id="userExperience" title="See Service Details and Prices">
         <i class="fa fa-bolt"></i>
         <div>User Experience Design &amp; Testing</div>
       </a>
-      <a href="construction.html" class="yes" id="SEO" title="See Service Details and Prices">
+      <a href="pricesAndServices.php" class="yes" id="SEO" title="See Service Details and Prices">
         <i class="fa fa-search"></i>
         <div>Search Engine Optimisation</div>
       </a>
-      <a href="construction.html" class="yes" id="advice" title="See Service Details and Prices">
+      <a href="pricesAndServices.php" class="yes" id="advice" title="See Service Details and Prices">
         <i class="fa fa-comment"></i>
         <div>Website &amp; Development Advice</div>
       </a>
